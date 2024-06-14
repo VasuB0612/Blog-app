14ae4bb5eb5e0c357c3148e92ab4211c1cebc05d
Vasu Bhatnagar <vasu.bhatnagar@tuni.fi>
2024-06-14 13:10:51 +0300

    working on the blog creation page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Signup from "./Pages/Signup";
 import Blogs from "./Pages/Blogs";
 import UserBlogs from "./Pages/UserBlogs";
 import Login from "./Pages/Login";
+import CreateBlog from "./Pages/CreateBlog";
 import { Routes, Route } from "react-router-dom";
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/blogs" element={<Blogs />} />
         <Route path="/myBlogs" element={<UserBlogs />} />
+        <Route path="/create" element={<CreateBlog />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Signup />} />
       </Routes>
--- frontend/src/Pages/CreateBlog.js
+++ frontend/src/Pages/CreateBlog.js
@@ -12,9 +12,25 @@ const CreateBlog = () => {
   };
   return (
     <div>
-      <form onSubmit={handleSubmit(e)}>
-        <Box width="80%">
-          <Typography variant="h2">Create a Post</Typography>
+      <form onSubmit={handleSubmit}>
+        <Box
+          width="60%"
+          border={3}
+          borderRadius={10}
+          padding={3}
+          margin="auto"
+          display="flex"
+          flexDirection="column"
+          marginTop="30px"
+        >
+          <Typography
+            variant="h2"
+            textAlign="center"
+            padding={3}
+            sx={{ fontFamily: "'Pixelify Sans', sans-serif" }}
+          >
+            Create a Post
+          </Typography>
         </Box>
       </form>
     </div>
