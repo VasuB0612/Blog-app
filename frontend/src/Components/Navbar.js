09e61ed4f059d12e9ca9ac79f87915a007884765
Vasu Bhatnagar <vasu.bhatnagar@tuni.fi>
2024-06-15 21:14:25 +0300

    Trying to make the navbar more responsive

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -13,6 +13,7 @@ import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useBlog } from "../Context/BlogProvider";
+import "../Styles/navStyles.css";
 const Navbar = () => {
   //  Global state
   const { user, setUser } = useBlog();
@@ -23,126 +24,194 @@ const Navbar = () => {
     navigate("/login");
   };
 
+  const navLinksStyle = {
+    display: "flex",
+    gap: "15px",
+    flexWrap: "wrap",
+  };
+
   // local state
   const [value, setValue] = useState();
   return (
-    <>
-      <AppBar
-        position="sticky"
-        sx={{
-          backgroundColor: "hotpink",
-        }}
-      >
-        <Toolbar>
-          <Avatar
-            src="https://cdn5.vectorstock.com/i/1000x1000/89/29/heart-pixel-pink-icon-isolated-on-black-vector-34538929.jpg"
-            alt="Logo"
-            sx={{ width: 32, height: 32, marginRight: 1, marginBottom: 0.5 }}
-          />
-          <Typography
-            variant="h4"
-            sx={{
-              color: "#745E96",
-              fontFamily: "'Pixelify Sans', sans-serif",
-              textTransform: "none",
-            }}
-          >
-            WeBlog
-          </Typography>
-          <Box display={"flex"} marginLeft="auto">
-            {user && (
-              <>
-                <Button
-                  label="Blogs"
-                  sx={{
-                    color: "#745E96",
-                    fontFamily: "'Pixelify Sans', sans-serif",
-                    marginRight: 2,
-                    textTransform: "none",
-                    fontSize: "17px",
-                  }}
-                  LinkComponent={Link}
-                  to="/blogs"
-                >
-                  Blogs
-                </Button>
-                <Button
-                  label="My Blogs"
-                  sx={{
-                    color: "#745E96",
-                    fontFamily: "'Pixelify Sans', sans-serif",
-                    marginRight: 2,
-                    textTransform: "none",
-                    fontSize: "17px",
-                  }}
-                  LinkComponent={Link}
-                  to="/myBlogs"
-                >
-                  My Blogs
-                </Button>
+    <nav>
+      {user && (
+        <ul>
+          <li>
+            <a href="#">Slog</a>
+          </li>
+          <li>
+            <a href="#">Blog</a>
+          </li>
+          <li>
+            <a href="#">My blogs</a>
+          </li>
+          <li>
+            <a href="#">Create</a>
+          </li>
+          <li onClick={handleLogout}>
+            <a href="#">Logout</a>
+          </li>
+        </ul>
+      )}
+      {!user && (
+        <ul>
+          <li>
+            <a href="#">Slog</a>
+          </li>
+          <li>
+            <a href="#">login</a>
+          </li>
+          <li>
+            <a href="#">sign up</a>
+          </li>
+        </ul>
+      )}
 
-                <Button
-                  label="My Blogs"
-                  sx={{
-                    color: "#745E96",
-                    fontFamily: "'Pixelify Sans', sans-serif",
-                    marginRight: 2,
-                    textTransform: "none",
-                    fontSize: "17px",
-                  }}
-                  LinkComponent={Link}
-                  to="/create"
-                >
-                  Create Blog
-                </Button>
-              </>
-            )}
-            {!user ? (
-              <>
-                <Button
-                  sx={{
-                    color: "#745E96",
-                    fontFamily: "'Pixelify Sans', sans-serif",
-                    marginRight: 2,
-                    textTransform: "none",
-                    fontSize: "17px",
-                  }}
-                  LinkComponent={Link}
-                  to="/login"
-                >
-                  Login
-                </Button>
-                <Button
-                  sx={{
-                    color: "#745E96",
-                    fontFamily: "'Pixelify Sans', sans-serif",
-                    marginRight: 4,
-                    textTransform: "none",
-                    fontSize: "17px",
-                  }}
-                  LinkComponent={Link}
-                  to="/register"
-                >
-                  Sign up
-                </Button>
-              </>
-            ) : (
-              <Button
-                sx={{
-                  color: "#745E96",
-                  fontFamily: "'Pixelify Sans', sans-serif",
-                  textTransform: "none",
-                  fontSize: "17px",
-                }}
-                onClick={handleLogout}
-              >
-                Logout
-              </Button>
-            )}
-          </Box>
-        </Toolbar>
-      </AppBar>
-    </>
+      {user && (
+        <ul class="sidebar">
+          <li>
+            <a href="#">Blog</a>
+          </li>
+          <li>
+            <a href="#">My blogs</a>
+          </li>
+          <li>
+            <a href="#">Create</a>
+          </li>
+          <li onClick={handleLogout}>
+            <a href="#">Logout</a>
+          </li>
+        </ul>
+      )}
+      {!user && (
+        <ul class="sidebar">
+          <li>
+            <a href="#">login</a>
+          </li>
+          <li>
+            <a href="#">sign up</a>
+          </li>
+        </ul>
+      )}
+    </nav>
+    // <>
+    //   <AppBar
+    //     position="sticky"
+    //     sx={{
+    //       backgroundColor: "hotpink",
+    //     }}
+    //     width="100%"
+    //   >
+    //     <Toolbar>
+    //       <Avatar
+    //         src="https://cdn5.vectorstock.com/i/1000x1000/89/29/heart-pixel-pink-icon-isolated-on-black-vector-34538929.jpg"
+    //         alt="Logo"
+    //         sx={{ width: 32, height: 32, marginRight: 1, marginBottom: 0.5 }}
+    //       />
+    //       <Typography
+    //         variant="h4"
+    //         sx={{
+    //           color: "#745E96",
+    //           fontFamily: "'Pixelify Sans', sans-serif",
+    //           textTransform: "none",
+    //         }}
+    //       >
+    //         WeBlog
+    //       </Typography>
+    //       <Box display={"flex"} marginLeft="auto">
+    //         {user && (
+    //           <>
+    //             <Button
+    //               label="Blogs"
+    //               sx={{
+    //                 color: "#745E96",
+    //                 fontFamily: "'Pixelify Sans', sans-serif",
+    //                 marginRight: 2,
+    //                 textTransform: "none",
+    //                 fontSize: "17px",
+    //               }}
+    //               LinkComponent={Link}
+    //               to="/blogs"
+    //             >
+    //               Blogs
+    //             </Button>
+    //             <Button
+    //               label="My Blogs"
+    //               sx={{
+    //                 color: "#745E96",
+    //                 fontFamily: "'Pixelify Sans', sans-serif",
+    //                 marginRight: 2,
+    //                 textTransform: "none",
+    //                 fontSize: "17px",
+    //               }}
+    //               LinkComponent={Link}
+    //               to="/myBlogs"
+    //             >
+    //               My Blogs
+    //             </Button>
+
+    //             <Button
+    //               label="My Blogs"
+    //               sx={{
+    //                 color: "#745E96",
+    //                 fontFamily: "'Pixelify Sans', sans-serif",
+    //                 marginRight: 2,
+    //                 textTransform: "none",
+    //                 fontSize: "17px",
+    //               }}
+    //               LinkComponent={Link}
+    //               to="/create"
+    //             >
+    //               Create Blog
+    //             </Button>
+    //           </>
+    //         )}
+    //         {!user ? (
+    //           <>
+    //             <Button
+    //               sx={{
+    //                 color: "#745E96",
+    //                 fontFamily: "'Pixelify Sans', sans-serif",
+    //                 marginRight: 2,
+    //                 textTransform: "none",
+    //                 fontSize: "17px",
+    //               }}
+    //               LinkComponent={Link}
+    //               to="/login"
+    //             >
+    //               Login
+    //             </Button>
+    //             <Button
+    //               sx={{
+    //                 color: "#745E96",
+    //                 fontFamily: "'Pixelify Sans', sans-serif",
+    //                 marginRight: 4,
+    //                 textTransform: "none",
+    //                 fontSize: "17px",
+    //               }}
+    //               LinkComponent={Link}
+    //               to="/register"
+    //             >
+    //               Sign up
+    //             </Button>
+    //           </>
+    //         ) : (
+    //           <Button
+    //             sx={{
+    //               color: "#745E96",
+    //               fontFamily: "'Pixelify Sans', sans-serif",
+    //               textTransform: "none",
+    //               fontSize: "17px",
+    //             }}
+    //             onClick={handleLogout}
+    //           >
+    //             Logout
+    //           </Button>
+    //         )}
+    //       </Box>
+    //     </Toolbar>
+    //   </AppBar>
+    // </>
   );
 };
 
