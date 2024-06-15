bd2de554ab27f7be1c2e093799a607f81acead2d
Vasu Bhatnagar <vasu.bhatnagar@tuni.fi>
2024-06-16 00:18:59 +0300

    Designing the blog creation page

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,15 +1,4 @@
 import React from "react";
-import {
-  Box,
-  AppBar,
-  Avatar,
-  Toolbar,
-  Button,
-  Typography,
-  Tabs,
-  Tab,
-} from "@mui/material";
-import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useBlog } from "../Context/BlogProvider";
@@ -17,19 +6,12 @@ import { IoMenuSharp } from "react-icons/io5";
 import { IoClose } from "react-icons/io5";
 import "../Styles/navStyles.css";
 const Navbar = () => {
-  //  Global state
   const { user, setUser } = useBlog();
   const navigate = useNavigate();
 
   const handleLogout = () => {
     setUser(localStorage.removeItem("userInfo"));
     navigate("/login");
-  };
-
-  const navLinksStyle = {
-    display: "flex",
-    gap: "15px",
-    flexWrap: "wrap",
   };
 
   const showSideBar = (e) => {
@@ -44,29 +26,27 @@ const Navbar = () => {
     sidebar.style.display = "none";
   };
 
-  // local state
-  const [value, setValue] = useState();
   return (
     <nav>
       {user && (
         <ul>
-          <li>
-            <a href="#">Slog</a>
+          <h1>
+            <a href={user ? "/myBlogs" : "#"}>Slog</a>
+          </h1>
+          <li class="hideOnSmallScreens">
+            <a href="/blogs">Blog</a>
           </li>
           <li class="hideOnSmallScreens">
-            <a href="#">Blog</a>
+            <a href="myBlogs">My blogs</a>
           </li>
           <li class="hideOnSmallScreens">
-            <a href="#">My blogs</a>
-          </li>
-          <li class="hideOnSmallScreens">
-            <a href="#">Create</a>
+            <a href="/create">Create</a>
           </li>
           <li class="hideOnSmallScreens" onClick={handleLogout}>
             <a href="#">Logout</a>
           </li>
           <li class="menu_button" onClick={showSideBar}>
-            <a href="">
+            <a href="#">
               <IoMenuSharp />
             </a>
           </li>
@@ -74,14 +54,14 @@ const Navbar = () => {
       )}
       {!user && (
         <ul>
+          <h1>
+            <a href={user ? "/myBlogs" : "#"}>Slog</a>
+          </h1>
           <li>
-            <a href="#">Slog</a>
+            <a href="/login">Login</a>
           </li>
           <li>
-            <a href="#">login</a>
-          </li>
-          <li>
-            <a href="#">sign up</a>
+            <a href="/register">Sign up</a>
           </li>
         </ul>
       )}
@@ -94,13 +74,13 @@ const Navbar = () => {
             </a>
           </li>
           <li>
-            <a href="#">Blog</a>
+            <a href="/blogs">Blog</a>
           </li>
           <li>
-            <a href="#">My blogs</a>
+            <a href="/myBlogs">My blogs</a>
           </li>
           <li>
-            <a href="#">Create</a>
+            <a href="/create">Create</a>
           </li>
           <li onClick={handleLogout}>
             <a href="#">Logout</a>
@@ -110,132 +90,14 @@ const Navbar = () => {
       {!user && (
         <ul class="sidebar">
           <li>
-            <a href="#">login</a>
+            <a href="/login">Login</a>
           </li>
           <li>
-            <a href="#">sign up</a>
+            <a href="/register">Sign up</a>
           </li>
         </ul>
       )}
     </nav>
-    // <>
-    //   <AppBar
-    //     position="sticky"
-    //     sx={{
-    //       backgroundColor: "hotpink",
-    //     }}
-    //     width="100%"
-    //   >
-    //     <Toolbar>
-    //       <Avatar
-    //         src="https://cdn5.vectorstock.com/i/1000x1000/89/29/heart-pixel-pink-icon-isolated-on-black-vector-34538929.jpg"
-    //         alt="Logo"
-    //         sx={{ width: 32, height: 32, marginRight: 1, marginBottom: 0.5 }}
-    //       />
-    //       <Typography
-    //         variant="h4"
-    //         sx={{
-    //           color: "#745E96",
-    //           fontFamily: "'Pixelify Sans', sans-serif",
-    //           textTransform: "none",
-    //         }}
-    //       >
-    //         WeBlog
-    //       </Typography>
-    //       <Box display={"flex"} marginLeft="auto">
-    //         {user && (
-    //           <>
-    //             <Button
-    //               label="Blogs"
-    //               sx={{
-    //                 color: "#745E96",
-    //                 fontFamily: "'Pixelify Sans', sans-serif",
-    //                 marginRight: 2,
-    //                 textTransform: "none",
-    //                 fontSize: "17px",
-    //               }}
-    //               LinkComponent={Link}
-    //               to="/blogs"
-    //             >
-    //               Blogs
-    //             </Button>
-    //             <Button
-    //               label="My Blogs"
-    //               sx={{
-    //                 color: "#745E96",
-    //                 fontFamily: "'Pixelify Sans', sans-serif",
-    //                 marginRight: 2,
-    //                 textTransform: "none",
-    //                 fontSize: "17px",
-    //               }}
-    //               LinkComponent={Link}
-    //               to="/myBlogs"
-    //             >
-    //               My Blogs
-    //             </Button>
-
-    //             <Button
-    //               label="My Blogs"
-    //               sx={{
-    //                 color: "#745E96",
-    //                 fontFamily: "'Pixelify Sans', sans-serif",
-    //                 marginRight: 2,
-    //                 textTransform: "none",
-    //                 fontSize: "17px",
-    //               }}
-    //               LinkComponent={Link}
-    //               to="/create"
-    //             >
-    //               Create Blog
-    //             </Button>
-    //           </>
-    //         )}
-    //         {!user ? (
-    //           <>
-    //             <Button
-    //               sx={{
-    //                 color: "#745E96",
-    //                 fontFamily: "'Pixelify Sans', sans-serif",
-    //                 marginRight: 2,
-    //                 textTransform: "none",
-    //                 fontSize: "17px",
-    //               }}
-    //               LinkComponent={Link}
-    //               to="/login"
-    //             >
-    //               Login
-    //             </Button>
-    //             <Button
-    //               sx={{
-    //                 color: "#745E96",
-    //                 fontFamily: "'Pixelify Sans', sans-serif",
-    //                 marginRight: 4,
-    //                 textTransform: "none",
-    //                 fontSize: "17px",
-    //               }}
-    //               LinkComponent={Link}
-    //               to="/register"
-    //             >
-    //               Sign up
-    //             </Button>
-    //           </>
-    //         ) : (
-    //           <Button
-    //             sx={{
-    //               color: "#745E96",
-    //               fontFamily: "'Pixelify Sans', sans-serif",
-    //               textTransform: "none",
-    //               fontSize: "17px",
-    //             }}
-    //             onClick={handleLogout}
-    //           >
-    //             Logout
-    //           </Button>
-    //         )}
-    //       </Box>
-    //     </Toolbar>
-    //   </AppBar>
-    // </>
   );
 };
 
--- frontend/src/Pages/CreateBlog.js
+++ frontend/src/Pages/CreateBlog.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, InputLabel, TextField, Button } from "@mui/material";
+import TextareaAutosize from "@mui/material/TextareaAutosize";
 
 const CreateBlog = () => {
   const [information, setInformation] = useState({
@@ -31,6 +32,52 @@ const CreateBlog = () => {
           >
             Create a Post
           </Typography>
+          <InputLabel sx={{ mb: 1, mt: 2, fontSize: "24px" }}>Title</InputLabel>
+          <TextField />
+          <InputLabel sx={{ mb: 1, mt: 2, fontSize: "24px" }}>
+            Image URL
+          </InputLabel>
+          <TextField />
+          <InputLabel sx={{ mb: 1, mt: 2, fontSize: "24px" }}>
+            Description
+          </InputLabel>
+          <textarea
+            placeholder="Write here"
+            name=""
+            id=""
+            rows={10}
+            columns={10}
+            style={{
+              background: "transparent",
+              fontFamily: "'Pixelify Sans', sans-serif",
+              fontSize: "20px",
+              paddingLeft: "10px",
+              paddingTop: "10px",
+              color: "black",
+            }}
+          ></textarea>
+          <button
+            style={{
+              marginTop: "7px",
+              textTransform: "none",
+              backgroundColor: "hotpink",
+              padding: "7px",
+              mt: 2,
+              borderRadius: "10px",
+              border: "2px dashed black",
+              transition: "background-color 0.5s ease",
+            }}
+            onMouseOver={(e) => {
+              e.target.style.backgroundColor = "black";
+              e.target.style.color = "hotpink";
+            }}
+            onMouseOut={(e) => {
+              e.target.style.backgroundColor = "hotpink";
+              e.target.style.color = "black";
+            }}
+          >
+            Submit
+          </button>
         </Box>
       </form>
     </div>
