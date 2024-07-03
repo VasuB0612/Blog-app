9b6c4db624f77201467912da7fc621004d8dd1ff
Vasu Bhatnagar <vasu.bhatnagar@tuni.fi>
2024-07-03 12:02:31 +0300

    Added styles

--- frontend/src/Components/HomePage.js
+++ frontend/src/Components/HomePage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
+import "../Styles/Home.css";
 
 const HomePage = () => {
   const [text] = useTypewriter({
@@ -13,28 +14,10 @@ const HomePage = () => {
     deleteSpeed: 50,
   });
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: "10%",
-        "@media (max-width: 600px)": {
-          marginTop: "20%",
-        },
-      }}
-    >
-      <h1 style={{ margin: "50px" }}>
-        <span
-          style={{
-            fontWeight: "bold",
-            color: "#1ac130",
-            alignItems: "center",
-          }}
-        >
-          {text}
-        </span>
-        <span style={{ color: "red" }}>
+    <div className="container">
+      <h1 className="heading">
+        <span className="text">{text}</span>
+        <span className="cursor">
           <Cursor />
         </span>
       </h1>
